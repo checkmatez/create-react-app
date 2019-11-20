f7c8406b13f6e74614e384add8fcb0a9de5a45a7
Max <davidovmk@yandex.ru>
2019-11-20 13:21:19 +0300

    fix: Disable chunks to produce single bundle

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -274,16 +274,17 @@ module.exports = function(webpackEnv) {
       // Automatically split vendor and commons
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      // Disable splitting and chunks to produce single bundle for easier microfrontend integration,
+      // https://github.com/camjackson/react-app-rewire-micro-frontends
       splitChunks: {
-        chunks: 'all',
-        name: false,
+        cacheGroups: {
+          default: false,
+        },
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
       // https://github.com/facebook/create-react-app/issues/5358
-      runtimeChunk: {
-        name: entrypoint => `runtime-${entrypoint.name}`,
-      },
+      runtimeChunk: false,
     },
     resolve: {
       // This allows you to set a fallback for where Webpack should look for modules.
